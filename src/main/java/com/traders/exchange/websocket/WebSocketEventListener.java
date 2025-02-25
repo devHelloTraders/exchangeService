74fc6e6df53a47b1876c10642eec2f478c5d0249
bunty <bunty.hada36@yahoo.com>
2025-02-25 18:36:17 +0530

    Client socket fix

--- src/main/java/com/traders/exchange/websocket/WebSocketEventListener.java
+++ src/main/java/com/traders/exchange/websocket/WebSocketEventListener.java
@@ -1,6 +1,5 @@
 package com.traders.exchange.websocket;
 
-import com.traders.exchange.webscoket.WebSocketSubscriptionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
